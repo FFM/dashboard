e02bc67777072426b21c38662b63e267da1914db
Aaron Kaplan <aaron@lo-res.org>
2013-05-01 23:45:14 +0200

    minor formatting

--- js/dashboard.js
+++ js/dashboard.js
@@ -57,7 +57,7 @@ $(document).ready(function() {
 		  mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-		console.log(map);
+		//console.log(map);
 
 		// google.maps.event.addDomListener(window, 'load', initialize);
 
