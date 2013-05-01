ddb9e24a98d7f14f96339bfb8d6f2f58375e540c
Aaron Kaplan <aaron@lo-res.org>
2013-05-01 19:10:01 +0200

    added google map - initial support

--- js/dashboard.js
+++ js/dashboard.js
@@ -48,6 +48,19 @@ $(document).ready(function() {
       $.get(this.template, function(t) {
         
         el.html(Mustache.render(t,{}));
+
+		// google maps
+		var map;
+		var mapOptions = {
+		  zoom: 8,
+		  center: new google.maps.LatLng(-34.397, 150.644),
+		  mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		console.log(map);
+
+		// google.maps.event.addDomListener(window, 'load', initialize);
+
         });
       }
    });
